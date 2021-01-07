845b1c44dc6056cce025a15d5dd6375160f723fd
Corgi <hwanggumedia@gmail.com>
2021-01-08 00:50:09 +0900

    feat: Add progress 'dark mode'

---
+++ dark-mode/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <label className="switch">
+      <input type="checkbox"/>
+      <span className="slider round"></span>
+    </label>
+  );
+}
+
+export default App;
