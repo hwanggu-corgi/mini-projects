927a50b967050084baa65554aca6602fb64ffef2
Corgi <hwanggumedia@gmail.com>
2021-01-08 06:02:08 +0900

    feat: Add progress #9 'dark mode'

--- dark-mode/src/App.js
+++ dark-mode/src/App.js
@@ -2,10 +2,19 @@ import './App.css';
 
 function App() {
   return (
-    <label className="switch">
-      <input type="checkbox"/>
-      <span className="slider round"></span>
-    </label>
+    <main>
+      <h1>Dark mode</h1>
+      <p>Dark mode can be set by changing to dark mode on Windows/Mac</p>
+      <ul>
+          <li>Windows: https://www.pcmag.com/how-to/how-to-enable-dark-mode-in-windows-10</li>
+          <li>Mac: https://css-tricks.com/a-complete-guide-to-dark-mode-on-the-web/</li>
+      </ul>
+      <p>Or by toggling the button below</p>
+      <label className="switch">
+          <input type="checkbox"/>
+          <span className="slider round"></span>
+      </label>
+    </main>
   );
 }
 
