572440d8a10cc2f5900aa6c8743825aec27d5c51
Corgi <hwanggumedia@gmail.com>
2021-01-09 02:02:26 +0900

    feat: Add progress #13 'image lazy loading'

--- image-lazy-loading/src/App.js
+++ image-lazy-loading/src/App.js
@@ -1,21 +1,45 @@
 import corgi from './assets/imgs/corgi.jpg';
 import './App.css';
 
-function App() {
-  return (
-    <main>
-        <img src={corgi}/>
-        <img src={corgi}/>
-        <img src={corgi}/>
-        <img src={corgi}/>
-        <img src={corgi}/>
-        <img src={corgi}/>
-        <img src={corgi}/>
-        <img src={corgi}/>
-        <img src={corgi}/>
-        <img src={corgi}/>
-    </main>
-  );
+import { Component } from 'react';
+
+class App extends Component {
+
+  componentDidMount() {
+    document.addEventListener("scroll", this.handleLazyLoading)
+  }
+
+  handleLazyLoading = e => {
+    const scroll = e.target.scrollingElement;
+    const main = document.querySelector("main");
+
+    // Check if it has reached the bottom of the page
+    // if it has reached the bottom of page, then load more images
+    if (scroll.scrollHeight <= scroll.scrollTop + scroll.clientHeight) {
+      for (let i = 0; i < 20; i++) {
+          let img = document.createElement("img");
+          img.src = "./corgi.jpg";
+          main.appendChild(img);
+      }
+    }
+  }
+
+  render() {
+    return (
+      <main>
+          <img src={corgi} alt="Corgi"/>
+          <img src={corgi} alt="Corgi"/>
+          <img src={corgi} alt="Corgi"/>
+          <img src={corgi} alt="Corgi"/>
+          <img src={corgi} alt="Corgi"/>
+          <img src={corgi} alt="Corgi"/>
+          <img src={corgi} alt="Corgi"/>
+          <img src={corgi} alt="Corgi"/>
+          <img src={corgi} alt="Corgi"/>
+          <img src={corgi} alt="Corgi"/>
+      </main>
+    );
+  }
 }
 
 export default App;
