1123bba942eccbc6154767b4e6f21d31027ecc14
Corgi <hwanggumedia@gmail.com>
2021-01-10 04:11:18 +0900

    feat: Add progress #23 'Learn Typescript'

--- learn-typescript/chapter_1/project_type_mart/index.ts
+++ learn-typescript/chapter_1/project_type_mart/index.ts
@@ -1,12 +1,24 @@
 import products from './products';
 
-let productName: string = products[1].name;
+let productName: string = products[3].name;
+let shipping: number;
+let shippingAddress: string = "123 Springfield Avenue";
+let taxPercent: number;
+let taxTotal: number;
+let total: number;
 
 let product = products.filter(item => {
   if (item.name == productName) {
     return true;
   }
 })[0];
+
+if (Number(product.price) >= 25) {
+  console.log("Free shipping being provided");
+  shipping = 0;
+} else {
+  shipping = 5;
+}
 
 if (product.preOrder == 'true') {
   console.log("we’ll send a message when it’s on the way");
