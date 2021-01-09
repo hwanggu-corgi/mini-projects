99b12fe06864190199fdf6980998d14e9c4bac5b
Corgi <hwanggumedia@gmail.com>
2021-01-10 04:27:02 +0900

    feat: Add progress #24 'Learn Typescript'

--- learn-typescript/chapter_1/project_type_mart/index.ts
+++ learn-typescript/chapter_1/project_type_mart/index.ts
@@ -2,7 +2,7 @@ import products from './products';
 
 let productName: string = products[3].name;
 let shipping: number;
-let shippingAddress: string = "123 Springfield Avenue";
+let shippingAddress: string = "123 Springfield Avenue, New York";
 let taxPercent: number;
 let taxTotal: number;
 let total: number;
@@ -20,6 +20,22 @@ if (Number(product.price) >= 25) {
   shipping = 5;
 }
 
+if (shippingAddress.match("New York")) {
+  taxPercent = 0.1;
+} else {
+  taxPercent = 0.05;
+}
+
+taxTotal = Number(product.price) * taxPercent;
+total = Number(product.price) + taxTotal + shipping;
+
 if (product.preOrder == 'true') {
   console.log("we’ll send a message when it’s on the way");
 }
+
+console.log(`Product name: ${product.name}`);
+console.log(`Shipping address: ${shippingAddress}`);
+console.log(`Price of the product: ${product.price}`);
+console.log(`Tax total: ${product.price}`);
+console.log(`Shipping: ${shipping}`);
+console.log(`Total amount: ${total}`);
