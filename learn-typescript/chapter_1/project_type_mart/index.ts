1832930f8663565fab7544b0ecf7cdcf28fc9aad
Corgi <hwanggumedia@gmail.com>
2021-01-10 02:09:51 +0900

    feat: Add progress #20 'Learn Typescript'

--- learn-typescript/chapter_1/project_type_mart/index.ts
+++ learn-typescript/chapter_1/project_type_mart/index.ts
@@ -2,10 +2,13 @@ import products from './products';
 
 let productName: string = products[0].name;
 
-let product = products.filter(item => {
+let product = products.find(item => {
   if (item.name == productName) {
     return true;
   }
 });
 
-console.log(product[0].name);
+console.log(Boolean(product.preOrder));
+if (Boolean(product.preOrder) == true) {
+  console.log("we’ll send a message when it’s on the way");
+}
