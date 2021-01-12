a072e348a85c2619a83426db62aa51ba4503834b
Corgi <hwanggumedia@gmail.com>
2021-01-13 02:04:09 +0900

    feat: Add progress #99 'Learn Typescript'

--- learn-typescript/chapter_3/project_restaurant_orders/solution/index.ts
+++ learn-typescript/chapter_3/project_restaurant_orders/solution/index.ts
@@ -13,7 +13,7 @@ let getMaxPrice = (price: PriceBracket) => {
 }
 
 /// Add your getOrders() function below:
-let getOrders: (price: PriceBracket, orders: Order[][]) => Order[][] = function(price: PriceBracket, orders: Order[][]) {
+let getOrders = function (price: PriceBracket, orders: Order[][]): Order[][] {
     let filteredOrders: Order[][] = [];
 
     orders.forEach(order => {
@@ -24,7 +24,22 @@ let getOrders: (price: PriceBracket, orders: Order[][]) => Order[][] = function(
 }
 
 /// Add your printOrders() function below:
+let printOrders = function(restaurants: Restaurant[], orders: Order[][]): void {
+    restaurants.forEach((restaurant, restaurantIndex) => {
+      if (orders[restaurantIndex].length == 0) {
+        return;
+      }
+
+      console.log(`Restaurant Name ${restaurant.name}`);
+
+      orders[restaurantIndex].forEach((order, orderIndex) => {
+        console.log(`Order ${orderIndex+1}: \$${order.price}`);
+      });
+
+    });
+}
 
 /// Main
-// const elligibleOrders = getOrders(PriceBracket.Low, orders);
+const elligibleOrders = getOrders(PriceBracket.Low, orders);
+console.log(elligibleOrders);
 // printOrders(restaurants, elligibleOrders);
