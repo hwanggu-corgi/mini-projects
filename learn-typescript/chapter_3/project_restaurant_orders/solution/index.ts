98eed2ae0ae7c8cb7e95d06560a9535863298c04
Corgi <hwanggumedia@gmail.com>
2021-01-13 01:16:03 +0900

    feat: Add progress #96 'Learn Typescript'

--- learn-typescript/chapter_3/project_restaurant_orders/solution/index.ts
+++ learn-typescript/chapter_3/project_restaurant_orders/solution/index.ts
@@ -3,10 +3,24 @@ import { orders, Order, PriceBracket } from "./orders";
 
 /// Add your getMaxPrice() function below:
 let getMaxPrice = (price: PriceBracket) => {
-  return
+  if (price == PriceBracket.Low) {
+    return 10.0
+  }
+
+  if (price == PriceBracket.Medium) {
+    return 20.0
+  }
+
+  if (price == PriceBracket.High) {
+    return 30.0
+  }
 }
 
 /// Add your getOrders() function below:
+let GetOrderFunction = (price: PriceBracket, orders:Order) => Order;
+getOrders: GetOrderFunction = (price: PriceBracket, orders: Order) => {
+
+}
 
 /// Add your printOrders() function below:
 
