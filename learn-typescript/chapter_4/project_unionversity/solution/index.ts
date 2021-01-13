e051791a60ee93ced8d5566bb5cafbf6702e4d24
Corgi <hwanggumedia@gmail.com>
2021-01-14 02:13:39 +0900

    feat: Add progress #126 'Learn Typescript'

---
+++ learn-typescript/chapter_4/project_unionversity/solution/index.ts
@@ -0,0 +1,27 @@
+import courses from './courses';
+import studyGroups from './studyGroup';
+
+type Course = {
+    id: number,
+    studyGroupId: number,
+    title: string,
+    keywords: string[],
+    eventType: string
+};
+
+type StudyGroup = {
+    id: number,
+    courseId: number,
+    title: string,
+    keywords: string[],
+    eventType: string
+};
+
+type SearchEventsOptions = {
+    query: string | number,
+    eventType: 'courses' | 'groups'
+};
+
+function searchEvents(options: SearchEventsOptions) {
+    let events: (Course | StudyGroup)[] = options.eventType == 'courses' ? courses : studyGroups;
+}
