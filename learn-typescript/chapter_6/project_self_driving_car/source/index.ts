0993895c393c4c35681da61aacaf265ec61d9322
Corgi <hwanggumedia@gmail.com>
2021-01-18 10:32:19 +0900

    feat: Add progress #181 'Learn Typescript'

--- learn-typescript/chapter_6/project_self_driving_car/source/index.ts
+++ learn-typescript/chapter_6/project_self_driving_car/source/index.ts
@@ -1,1 +1,16 @@
 import { getObstacleEvents } from './computer-vision';
+
+interface AutonomousCar {
+  isRunning?: boolean
+}
+
+interface AutonomousCarProps {
+  isRunning?: boolean
+}
+
+class Car implements AutonomousCar {
+  isRunning;
+  constructor(props: AutonomousCarProps) {
+    this.isRunning = props.isRunning;
+  }
+}
