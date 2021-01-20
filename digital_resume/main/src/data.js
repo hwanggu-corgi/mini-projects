ce28c0a59477fa5b725816c1415bd7799679a192
Corgi <hwanggumedia@gmail.com>
2021-01-20 10:08:13 +0900

    feat: Add progress #28 'Digital Resume'

---
+++ digital_resume/main/src/data.js
@@ -0,0 +1,10 @@
+export const data = {
+    "highlights": [
+        "Emerging web developer with over 2 years of hands-on experience",
+        "Developed HTML templates from mockups that are both pixel-perfect and responsive",
+        "Strong skills in HTML5, CSS3 and ES6 JavaScript and comfort in React",
+        "Strong ability to work independently and in collaboration with team members",
+        "Committed to refining front-end skills through Codecademy’s Learn Typescript Course"
+    ]
+
+};
