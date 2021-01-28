d6ef3b8d4a7f09065271932aa02fb044defc2123
Corgi <hwanggumedia@gmail.com>
2021-01-29 00:39:02 +0900

    feat: Add progress #31 'graph-ql'

--- graph-ql/server/src/index.js
+++ graph-ql/server/src/index.js
@@ -1,23 +1,60 @@
 const { ApolloServer } = require('apollo-server');
+const fs = require('fs');
+const path = require('path');
 
 // 1
+// is useful for small server
+// const typeDefs = `
+//   type Query {
+//     info: String!
+//     feed: [Link!]!
+//   }
+
+//   type Mutation {
+//     post(url: String!, description: String!): Link!
+//   }
+
+//   type Link {
+//     id: ID!
+//     description: String!
+//     url: String!
+//   }
+// `
+
 let links = [{
     id: 'link-0',
     url: 'www.howtographql.com',
     description: 'Fullstack tutorial for GraphQL'
-  }]
+}]
 
 const resolvers = {
-Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    // 2
-    feed: () => links,
-},
+    Query: {
+        info: () => `This is the API of a Hackernews Clone`,
+        feed: () => links,
+    },
+    Mutation: {
+        post: (parent, args) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url,
+            }
+            links.push(link)
+            return link
+        }
+    },
+}
 
-// 3
-Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url,
-}
-}
+const server = new ApolloServer({
+    typeDefs: fs.readFileSync(
+        path.join(__dirname, 'schema.graphql'),
+        'utf8'
+    ),
+    resolvers,
+})
+
+server
+.listen()
+.then(({ url }) =>
+    console.log(`Server is running on ${url}`)
+);
