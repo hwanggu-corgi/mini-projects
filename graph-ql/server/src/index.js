58e39460a0328d91b16fff6fd35d6372c54afe3d
Corgi <hwanggumedia@gmail.com>
2021-01-28 14:08:26 +0900

    feat: Add progress #29 'graph-ql'

--- graph-ql/server/src/index.js
+++ graph-ql/server/src/index.js
@@ -4,24 +4,32 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `
 
-// 2
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  }]
+
 const resolvers = {
-  Query: {
-    info: () => null
-  }
-}
-
+Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    // 2
+    feed: () => links,
+},
 // 3
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-})
-
-server
-  .listen()
-  .then(({ url }) =>
-    console.log(`Server is running on ${url}`)
-  );
+Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
+}
+}
