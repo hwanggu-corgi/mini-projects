a5c424732dd02468ce0bd61a2bf2a058d3b5ceb0
Corgi <hwanggumedia@gmail.com>
2021-01-29 04:18:16 +0900

    feat: Add progress #39 'graph-ql'

--- graph-ql/server/src/script.js
+++ graph-ql/server/src/script.js
@@ -5,8 +5,14 @@ const prisma = new PrismaClient()
 
 //3
 async function main() {
-  const allLinks = await prisma.link.findMany()
-  console.log(allLinks)
+    const newLink = await prisma.link.create({
+        data: {
+            description: 'Fullstack tutorial for GraphQL',
+            url: 'www.howtographql.com'
+        }
+    });
+    const allLinks = await prisma.link.findMany()
+    console.log(allLinks)
 }
 
 //4
