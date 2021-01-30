ad3ea64e0330dd4fe3490ef3dbee56367ea9b64f
Corgi <hwanggumedia@gmail.com>
2021-01-31 02:13:06 +0900

    feat: Add progress #80 'graph-ql'

--- graph-ql/main/src/index.js
+++ graph-ql/main/src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
-
-// 1
 import {
   ApolloProvider,
   ApolloClient,
   createHttpLink,
   InMemoryCache
 } from '@apollo/client';
+import { BrowserRouter } from 'react-router-dom';
+
+import './styles/index.css';
+import App from './components/App';
+import * as serviceWorker from './serviceWorker';
 
 // 2
 const httpLink = createHttpLink({
@@ -25,9 +25,11 @@ const client = new ApolloClient({
 
 // 4
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 serviceWorker.unregister();
