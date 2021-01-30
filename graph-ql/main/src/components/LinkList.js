b62c91acbf82207234f9e09c61677365030b7dba
Corgi <hwanggumedia@gmail.com>
2021-01-31 00:44:00 +0900

    feat: Add progress #76 'graph-ql'

--- graph-ql/main/src/components/LinkList.js
+++ graph-ql/main/src/components/LinkList.js
@@ -1,28 +1,37 @@
+import { useQuery, gql } from '@apollo/client';
 import React from 'react';
 import Link from './Link';
 
-const LinkList = () => {
-  const linksToRender = [
-    {
-      id: '1',
-      description:
-        'Prisma gives you a powerful database toolkit 😎',
-      url: 'https://prisma.io'
-    },
-    {
-      id: '2',
-      description: 'The best GraphQL client',
-      url: 'https://www.apollographql.com/docs/react/'
+// <> </> means React.Fragment
+
+const FEED_QUERY = gql`
+  {
+    feed {
+      id
+      links {
+        id
+        createdAt
+        url
+        description
+      }
     }
-  ];
+  }
+`;
 
-  return (
-    <div>
-      {linksToRender.map((link) => (
-        <Link key={link.id} link={link} />
-      ))}
-    </div>
-  );
+
+const LinkList = () => {
+    const { data } = useQuery(FEED_QUERY);
+
+    return (
+        <div>
+        {data && (
+            <>
+            {data.feed.links.map((link) => (
+                <Link key={link.id} link={link} />
+            ))}
+            </>
+        )}
+        </div>
+    );
 };
-
 export default LinkList;
