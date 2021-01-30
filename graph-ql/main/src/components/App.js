2cab1f649b216bb71eb1e4cee9191eeb7649bb80
Corgi <hwanggumedia@gmail.com>
2021-01-31 08:58:17 +0900

    feat: Add progress #91 'graph-ql'

--- graph-ql/main/src/components/App.js
+++ graph-ql/main/src/components/App.js
@@ -5,23 +5,26 @@ import LinkList from './LinkList';
 import { Switch, Route } from 'react-router-dom';
 import Login from './Login';
 
-const App = () => {
-  return (
-    <div className="center w85">
-      <Header />
-      <div className="ph3 pv1 background-gray">
-        <Switch>
-          <Route exact path="/" component={LinkList} />
-          <Route
-            exact
-            path="/create"
-            component={CreateLink}
-          />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </div>
-    </div>
-  );
-};
+const App = () => (
+  <div className="center w85">
+    <Header />
+    <div className="ph3 pv1 background-gray">
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => <Redirect to="/new/1" />}
+        />
 
+        <Route
+          exact
+          path="/create"
+          component={CreateLink}
+        />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/search" component={Search} />
+      </Switch>
+    </div>
+  </div>
+);
 export default App;
