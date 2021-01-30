5f3bca68019f9434ae23f7dedd3f89232bd4862e
Corgi <hwanggumedia@gmail.com>
2021-01-31 05:44:16 +0900

    feat: Add progress #86 'graph-ql'

--- graph-ql/main/src/components/Link.js
+++ graph-ql/main/src/components/Link.js
@@ -1,14 +1,38 @@
-import React from 'react';
 
 const Link = (props) => {
-  const { link } = props;
-  return (
-    <div>
-      <div>
-        {link.description} ({link.url})
-      </div>
-    </div>
-  );
-};
+    const { link } = props;
+    const authToken = localStorage.getItem(AUTH_TOKEN);
 
-export default Link;
+    const take = LINKS_PER_PAGE;
+    const skip = 0;
+    const orderBy = { createdAt: 'desc' };
+
+    return (
+      <div className="flex mt2 items-start">
+        <div className="flex items-center">
+          <span className="gray">{props.index + 1}.</span>
+          {authToken && (
+            <div
+              className="ml1 gray f11"
+              style={{ cursor: 'pointer' }}
+              onClick={vote}
+            >
+              ▲
+            </div>
+          )}
+        </div>
+        <div className="ml1">
+          <div>
+            {link.description} ({link.url})
+          </div>
+          {authToken && (
+            <div className="f6 lh-copy gray">
+              {link.votes.length} votes | by{' '}
+              {link.postedBy ? link.postedBy.name : 'Unknown'}{' '}
+              {timeDifferenceForDate(link.createdAt)}
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  };
