bc949350a26dc92d73ce98acb9234093e005f72a
Corgi <hwanggumedia@gmail.com>
2021-01-31 13:52:13 +0900

    feat: Add progress #97 'graph-ql'

--- graph-ql/main/src/components/LinkList.js
+++ graph-ql/main/src/components/LinkList.js
@@ -4,6 +4,28 @@ import Link from './Link';
 
 // <> </> means React.Fragment
 
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+      createdAt
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`;
+
+
 export const FEED_QUERY = gql`
   {
     feed {
@@ -59,7 +81,23 @@ const LinkList = () => {
   });
 
   subscribeToMore({
-    // ...
+    document: NEW_LINKS_SUBSCRIPTION,
+    updateQuery: (prev, { subscriptionData }) => {
+      if (!subscriptionData.data) return prev;
+      const newLink = subscriptionData.data.newLink;
+      const exists = prev.feed.links.find(
+        ({ id }) => id === newLink.id
+      );
+      if (exists) return prev;
+
+      return Object.assign({}, prev, {
+        feed: {
+          links: [newLink, ...prev.feed.links],
+          count: prev.feed.links.length + 1,
+          __typename: prev.feed.__typename
+        }
+      });
+    }
   });
 
   // ...
