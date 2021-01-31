6ca57ce66209a070c9ab3e057f4633f9d5298b72
Corgi <hwanggumedia@gmail.com>
2021-01-31 10:32:54 +0900

    feat: Add progress #94 'graph-ql'

--- graph-ql/main/src/components/Search.js
+++ graph-ql/main/src/components/Search.js
@@ -28,23 +28,34 @@ const FEED_SEARCH_QUERY = gql`
 `;
 
 const Search = () => {
-  const [searchFilter, setSearchFilter] = useState('');
-  return (
-    <>
-      <div>
-        Search
-        <input
-          type="text"
-          onChange={(e) => setSearchFilter(e.target.value)}
-        />
-        <button>OK</button>
-      </div>
-      {data &&
-        data.feed.links.map((link, index) => (
-          <Link key={link.id} link={link} index={index} />
-        ))}
-    </>
-  );
-};
+    const [searchFilter, setSearchFilter] = useState('');
+    const [executeSearch, { data }] = useLazyQuery(
+      FEED_SEARCH_QUERY
+    );
+    return (
+      <>
+        <div>
+          Search
+          <input
+            type="text"
+            onChange={(e) => setSearchFilter(e.target.value)}
+          />
+          <button
+            onClick={() =>
+              executeSearch({
+                variables: { filter: searchFilter }
+              })
+            }
+          >
+            OK
+          </button>
+        </div>
+        {data &&
+          data.feed.links.map((link, index) => (
+            <Link key={link.id} link={link} index={index} />
+          ))}
+      </>
+    );
+  };
 
 export default Search;
