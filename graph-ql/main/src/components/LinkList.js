a73349f4fa63bf0ad236faf579af6d0e7d153842
Corgi <hwanggumedia@gmail.com>
2021-01-31 13:39:48 +0900

    feat: Add progress #96 'graph-ql'

--- graph-ql/main/src/components/LinkList.js
+++ graph-ql/main/src/components/LinkList.js
@@ -28,20 +28,40 @@ export const FEED_QUERY = gql`
   }
 `;
 
-const LinkList = () => {
-  const { data } = useQuery(FEED_QUERY);
-
-  return (
-    <div>
-      {data && (
-        <>
-          {data.feed.links.map((link, index) => (
-            <Link key={link.id} link={link} index={index} />
-          ))}
-        </>
-      )}
-    </div>
-  );
+const getQueryVariables = (isNewPage, page) => {
+  const skip = isNewPage ? (page - 1) * LINKS_PER_PAGE : 0;
+  const take = isNewPage ? LINKS_PER_PAGE : 100;
+  const orderBy = { createdAt: 'desc' };
+  return { take, skip, orderBy };
 };
 
+const LinkList = () => {
+  const history = useHistory();
+  const isNewPage = history.location.pathname.includes(
+    'new'
+  );
+  const pageIndexParams = history.location.pathname.split(
+    '/'
+  );
+  const page = parseInt(
+    pageIndexParams[pageIndexParams.length - 1]
+  );
+
+  const pageIndex = page ? (page - 1) * LINKS_PER_PAGE : 0;
+
+  const {
+    data,
+    loading,
+    error,
+    subscribeToMore
+  } = useQuery(FEED_QUERY, {
+    variables: getQueryVariables(isNewPage, page)
+  });
+
+  subscribeToMore({
+    // ...
+  });
+
+  // ...
+};
 export default LinkList;
