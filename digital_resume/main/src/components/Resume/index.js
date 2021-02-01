d6bdc47adc2c367ff37134634305428adc67e4f7
Corgi <hwanggumedia@gmail.com>
2021-02-02 01:18:43 +0900

    feat: Add progress #5 'digital resume'

--- digital_resume/main/src/components/Resume/index.js
+++ digital_resume/main/src/components/Resume/index.js
@@ -1,144 +1,53 @@
 import styled from 'styled-components';
-import { data } from '../../data';
 
+import { data } from './data';
+import { WorkExpItem, ProjectExpItem,
+         EducationItem, ResumeHeader } from './components/Resume/lib';
 
-const Article = styled.article`
-  margin-bottom: 1em;
-`;
+function Resume() {
+  const Main = styled.section`
+    font-family: Arial, Helvetica, sans-serif;
+    max-width: 700px;
+    margin: 0 auto;
+    padding: 20px;
+  `;
 
-const Div = styled.div`
-  &:first-child {
-    flex-grow: 1;
-  }
-`;
-
-const Header = styled.header`
-  @media only screen and (min-width: 660px) {
-    display: flex;
-  }
-`;
-
-const Hr = styled.hr`
-  border-color: black;
-  border-width: 2px;
-`;
-
-const H1 = styled.h1`
-  margin-bottom: 0.5em;
-  margin-top: 0;
-`;
-
-const H3 = styled.h3`
-  margin-bottom: 0.5em;
-  margin-top: 0;
-`;
-
-const P = styled.p`
-  margin-bottom: 0;
-  margin-top: 0;
-`;
-
-const Ul = styled.ul`
-  padding-left: 20px;
-`;
-
-
-export function EducationItem(props) {
-  return (
-    <Article>
-      <Header>
-        <Div>
-          <H3>{props.name}</H3>
-          <P>{props.location}</P>
-        </Div>
-        <Div>
-          <P>{props.date}</P>
-        </Div>
-      </Header>
-    </Article>
-  );
-}
-
-
-export function ProjectExpItem(props) {
-  return (
-    <Article>
-      <Header>
-        <Div>
-          <H3>{props.name}</H3>
-          <a href={props.url}>{props.url}</a>
-        </Div>
-        <Div>
-            <P>{props.date}</P>
-        </Div>
-      </Header>
-      <Ul>
-        {props.details.map(detail => <li>{detail}</li>)}
-      </Ul>
-    </Article>
-  );
-}
-
-export function WorkExpItem(props) {
-  return (
-    <Article>
-      <Header>
-        <Div>
-          <H3>{props.title}</H3>
-          <P>{props.location}</P>
-        </Div>
-        <Div>
-          <P>{props.date}</P>
-        </Div>
-      </Header>
-      <Ul>
-        {props.details.map(detail => <li>{detail}</li>)}
-      </Ul>
-    </Article>
-  );
-}
-
-
-export function ResumeHeader(props) {
-  const Header = styled.header`
-    @media only screen and (min-width: 730px) {
-      text-align: center;
-    }
+  const H2 = styled.h2`
+    margin-bottom: 0.5em;
+    margin-top: 0;
   `;
 
   const Ul = styled.ul`
-    list-style: none;
-    padding: 0;
-
-    @media only screen and (min-width: 730px) {
-      display: flex;
-    }
-  `;
-
-  const Li = styled.li`
-    flex-grow: 1;
-
-    &:last-child {
-      border-right: none;
-    }
-
-    @media only screen and (min-width: 730px) {
-      flex-grow: 1;
-      border-right: black solid 1px;
-    }
+    padding-left: 20px;
   `;
 
   return (
-    <Header>
-      <H1>Hyungmo (Moe) Gu</H1>
-      <P>Calgary, AB, Canada</P>
-      <Ul>
-        <Li>Web: <a href={data.info.web.url}>{data.info.web.label}</a></Li>
-        <Li>LinkedIn: <a href={data.info.linkedIn.url}>{data.info.linkedIn.label}</a></Li>
-        <Li><a href={'mailto:' + data.info.email}>{data.info.email}</a></Li>
-        <Li>T: {data.info.tel}</Li>
-      </Ul>
-      <Hr></Hr>
-    </Header>
+    <Main>
+        <ResumeHeader/>
+        <section className="qualification">
+            <H2><u>Highlights of Qualification</u></H2>
+            <Ul>
+              {data.highlights.map(item => <li>{item}</li>)}
+            </Ul>
+        </section>
+        <section>
+            <H2><u>Technical Skills</u></H2>
+            <p>{data.techSkills.join(", ")}</p>
+        </section>
+        <section className="work-experience">
+            <H2><u>Work Experience</u></H2>
+            {data.workExp.map(item => <WorkExpItem {...item} />)}
+        </section>
+        <section className="project-experience">
+            <H2><u>Project Experience</u></H2>
+            {data.projectExp.map(item => <ProjectExpItem {...item} />)}
+        </section>
+        <section className="education">
+            <H2><u>Education</u></H2>
+            {data.education.map(item => <EducationItem {...item} />)}
+        </section>
+    </Main>
   );
 }
+
+export default Resume;
