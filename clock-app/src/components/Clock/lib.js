386b04e623cefc72aad4eab526d305da0ebf6510
Corgi <hwanggumedia@gmail.com>
2021-02-03 06:05:04 +0900

    feat: Add progress #11 'clock app'

--- clock-app/src/components/Clock/lib.js
+++ clock-app/src/components/Clock/lib.js
@@ -2,13 +2,20 @@ import styled from 'styled-components';
 
 export function ExpandButton() {
     let Button = styled.button`
+        cursor: pointer;
+        box-shadow: transparent;
         background-color: white;
+        line-height: 1.75em;
+        letter-spacing: 0.3125em;
         text-transform: uppercase;
-        padding: 0.75em;
+        padding: 0.5625em;
+        border-style: solid;
+        border-color: #979797;
+        border-radius: 50px 50px;
     `;
     return (
         <Button>
-            More <span>^</span>
+            <strong>More</strong><span>^</span>
         </Button>
     );
   }
