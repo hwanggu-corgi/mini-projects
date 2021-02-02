dbaf645bf56cd9db87f0bf73d3fa39b6548754a5
Corgi <hwanggumedia@gmail.com>
2021-02-03 08:55:27 +0900

    feat: Add progress #16 'clock app'

--- clock-app/src/components/Clock/lib.js
+++ clock-app/src/components/Clock/lib.js
@@ -1,20 +1,26 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
+let borderColor = "#979797";
 let hoveredColor = "#999999";
 
 export function ExpandButton() {
+    const [toggled, setToggle] = useState(false);
     let Button = styled.button`
         cursor: pointer;
-        background-color: white;
+        width: 146px;
         line-height: 1.75em;
         letter-spacing: 0.3125em;
+        background-color: white;
         text-transform: uppercase;
         padding: 0.5625em;
         border-style: solid;
-        border-color: #979797;
+        border-color: ${borderColor};
         border-radius: 50px 50px;
         display: flex;
+        justify-content: flex-end;
         align-items:center;
+
         &:hover{
             span {
                 color: ${hoveredColor};
@@ -30,8 +36,8 @@ export function ExpandButton() {
     `;
 
     return (
-        <Button>
-            <Span>More</Span>
+        <Button onClick={ _ => setToggle(!toggled)}>
+            <Span>{toggled ? "Less" : "More"}</Span>
             <svg width="40" height="40" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><circle fill="#303030" cx="20" cy="20" r="20"/><path stroke="#FFF" stroke-width="2" d="M14 23l6-6 6 6"/></g></svg>
         </Button>
     );
