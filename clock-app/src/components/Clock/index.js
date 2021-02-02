05a4e7f1d3b0954c864b2f22f915b155e1ec6986
Corgi <hwanggumedia@gmail.com>
2021-02-03 04:16:33 +0900

    feat: Add progress #8 'clock app'

--- clock-app/src/components/Clock/index.js
+++ clock-app/src/components/Clock/index.js
@@ -9,12 +9,14 @@ let H1 = styled.h1`
     line-height: 200px;
     font-weight: bold;
     letter-spacing: -5px;
+    margin: 1rem 0;
 `;
 
 let H2 = styled.h2`
     font-size: 56px;
     line-height: 68px;
     letter-spacing: 5px;
+    margin: 1rem 0;
 `;
 
 let H3 = styled.h3`
@@ -23,6 +25,7 @@ let H3 = styled.h3`
     line-height: 28px;
     letter-spacing: 4.8px;
     text-transform: uppercase;
+    margin: 1rem 0;
 `;
 
 let H4 = styled.h4`
@@ -30,6 +33,24 @@ let H4 = styled.h4`
     font-weight: normal;
     line-height: 28px;
     letter-spacing: 4px;
+    text-transform: uppercase;
+    margin: 1rem 0;
+`;
+
+let H5 = styled.h5`
+    font-size: 18px;
+    font-weight: bold;
+    line-height: 28px;
+    margin: 1rem 0;
+`;
+
+let H6 = styled.h6`
+    font-size: 15px;
+    font-weight: normal;
+    line-height: 28px;
+    letter-spacing: 3px;
+    text-transform: uppercase;
+    margin: 1rem 0;
 `;
 
 
@@ -40,6 +61,8 @@ function Clock() {
             <H2>Lorem ipsum dolor sit</H2>
             <H3>Lorem ipsum dolor sit amet, consectetuer adipiscing elit</H3>
             <H4>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros.</H4>
+            <H5>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.</H5>
+            <H6>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros.</H6>
             <section>
 
 
