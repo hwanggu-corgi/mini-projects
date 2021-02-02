b5dc0774b93a277f5af2a944b26100d4c551c6b8
Corgi <hwanggumedia@gmail.com>
2021-02-03 02:01:14 +0900

    feat: Add progress #3 'clock app'

---
+++ clock-app/src/components/Clock/index.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+let color_primary = '#000000';
+let color_secondary = '#303030';
+let color_white = '#FFFFFF';
+
+function Clock() {
+  return (
+    <section>
+    , it's currently
+
+    in
+
+    More
+    Less
+
+    Current timezone
+    Day of the year
+    Day of the week
+    Week number
+
+    </section>
+  );
+}
+
+export default Clock;
