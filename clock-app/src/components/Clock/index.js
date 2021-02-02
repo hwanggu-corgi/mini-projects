c23b8a9ddcab5156898a59d52344b7ac2dccc5e5
Corgi <hwanggumedia@gmail.com>
2021-02-03 02:15:15 +0900

    feat: Add progress #4 'clock app'

--- clock-app/src/components/Clock/index.js
+++ clock-app/src/components/Clock/index.js
@@ -4,22 +4,43 @@ let color_primary = '#000000';
 let color_secondary = '#303030';
 let color_white = '#FFFFFF';
 
+let H1 = styled.h1`
+    font-size: 200px;
+    line-height: 200px;
+    font-weight: bold;
+    line-spacing: 5px;
+`;
+
+let H2 = styled.h2`
+    font-size: 56px;
+    line-height: 68px;
+    line-spacing: 5px;
+`;
+
+let H3 = styled.h3`
+`;
+
 function Clock() {
   return (
-    <section>
-    , it's currently
+      <div>
+            <H1>Lorem</H1>
+            <section>
 
-    in
 
-    More
-    Less
+            , it's currently
 
-    Current timezone
-    Day of the year
-    Day of the week
-    Week number
+            in
 
-    </section>
+            More
+            Less
+
+            Current timezone
+            Day of the year
+            Day of the week
+            Week number
+
+            </section>
+        </div>
   );
 }
 
