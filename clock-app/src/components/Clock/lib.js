648843ae0802ca1a1b55958dbd6f5a83124fe88b
Corgi <hwanggumedia@gmail.com>
2021-02-03 08:03:40 +0900

    feat: Add progress #15 'clock app'

--- clock-app/src/components/Clock/lib.js
+++ clock-app/src/components/Clock/lib.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
-import { IconArrowUp } from './icons';
+
+let hoveredColor = "#999999";
 
 export function ExpandButton() {
     let Button = styled.button`
@@ -14,23 +15,24 @@ export function ExpandButton() {
         border-radius: 50px 50px;
         display: flex;
         align-items:center;
+        &:hover{
+            span {
+                color: ${hoveredColor};
+            }
+            circle {
+                fill: ${hoveredColor};
+            }
+        }
     `;
 
     let Span = styled.span`
         margin: 0 0.875em;
     `;
 
-    let Svg = styled.svg`
-        fill: red;
-        &:hover{
-            fill: #999999;
-        }
-    `;
-
     return (
         <Button>
             <Span>More</Span>
-            <Svg width="40" height="40" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><circle fill="#303030" cx="20" cy="20" r="20"/><path stroke="#FFF" stroke-width="2" d="M14 23l6-6 6 6"/></g></Svg>
+            <svg width="40" height="40" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><circle fill="#303030" cx="20" cy="20" r="20"/><path stroke="#FFF" stroke-width="2" d="M14 23l6-6 6 6"/></g></svg>
         </Button>
     );
   }
