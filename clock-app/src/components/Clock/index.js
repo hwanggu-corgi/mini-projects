77be034e8942be3004c4612f0982b349ceac57b0
Corgi <hwanggumedia@gmail.com>
2021-02-05 03:04:48 +0900

    feat: Add progress #24 'Clock App'

--- clock-app/src/components/Clock/index.js
+++ clock-app/src/components/Clock/index.js
@@ -138,29 +138,43 @@ function ExpandButton() {
 }
 
 function Quote() {
-    let Article = styled.article`
+    const Article = styled.article`
         max-width: 573px;
+        display: flex;
+    `;
+
+    const Blockquote = styled.blockquote`
+        margin: 0 10.67px 0 0;
     `;
 
     return (
         <Article>
-            <blockquote>
+            <Blockquote>
                 <P>The science of operations, as derived from mathematics more especially, is a science of itself, and has its own abstract truth and value.</P>
                 <H5>Ada Lovelace</H5>
-            </blockquote>
-            <Refresh/>
+            </Blockquote>
+            <section>
+                <Refresh/>
+            </section>
         </Article>
     );
 }
 
 function Refresh() {
+    const Button = styled.button`
+        border: none;
+        background-color: transparent;
+        cursor: pointer;
+        padding: 0;
+    `;
+
     const Path = styled.path`
         fill: ${refershIconColor}
     `;
 
     return (
-        <button>
+        <Button>
             <svg width="18" height="18" xmlns="http://www.w3.org/2000/svg"><Path d="M7.188 10.667a.208.208 0 01.147.355l-2.344 2.206a5.826 5.826 0 009.578-2.488l2.387.746A8.322 8.322 0 013.17 14.94l-2.149 2.022a.208.208 0 01-.355-.148v-6.148h6.52zm7.617-7.63L16.978.958a.208.208 0 01.355.146v6.23h-6.498a.208.208 0 01-.147-.356L13 4.765A5.825 5.825 0 003.43 7.26l-2.386-.746a8.32 8.32 0 0113.76-3.477z" fill="#FFF" fill-rule="nonzero" opacity=".5"/></svg>
-        </button>
+        </Button>
     );
 }
