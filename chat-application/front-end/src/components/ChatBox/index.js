703e88ecde7df5e9a7629dd72a27531d936c8674
Corgi <hwanggumedia@gmail.com>
2021-02-22 03:24:01 +0900

    feat: Add progress #17 'Chat Application'

--- chat-application/front-end/src/components/ChatBox/index.js
+++ chat-application/front-end/src/components/ChatBox/index.js
@@ -4,29 +4,15 @@ import styled from 'styled-components';
 const colorGrey = "#EEEEEE";
 
 export function ChatBox() {
-    const Div = styled.div`
-        background-color: ${colorGrey};
-        padding: 1em;
-        border-radius: 5em;
-        display:flex;
+    const Article = styled.article`
+
     `;
 
-    const Textarea = styled.textarea`
-        border: none;
-        resize: none;
-        width: 100%;
-        background-color: ${colorGrey};
-        text-decoration: none;
-        padding: 1em;
-        overflow-y: scroll;
-
-        &:focus {
-            outline: none;
-        }
-    `;
     return (
-        <Div>
-            <Textarea/>
-        </Div>
+        <Article>
+            <Header>
+            </Header>
+            <P></P>
+        </Article>
     );
 }
