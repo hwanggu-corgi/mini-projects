217bc62aa043a122f19ee25bd5dab412f87f7aa7
Corgi <hwanggumedia@gmail.com>
2021-02-22 08:05:03 +0900

    feat: Add progress #23 'Chat Application'

--- chat-application/front-end/src/components/Navigation/index.js
+++ chat-application/front-end/src/components/Navigation/index.js
@@ -4,9 +4,27 @@ import styled from 'styled-components';
 const colorOrange = "#FFA341";
 
 export function MobileNavigation() {
-    return (
-        <nav>
+    const Nav = styled.nav`
+        padding: 1em;
+        background-color: ${colorOrange};
+    `;
 
-        </nav>
+    const Div = styled.div`
+        background-color: white;
+        width: 2em;
+        height: 0.2em;
+        margin-bottom: 0.5em;
+
+        &:last-child {
+            margin: 0;
+        }
+    `;
+
+    return (
+        <Nav>
+            <Div></Div>
+            <Div></Div>
+            <Div></Div>
+        </Nav>
     );
 }
