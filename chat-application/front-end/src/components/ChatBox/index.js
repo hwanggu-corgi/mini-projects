795c94182329819b66edba1681bb1ee477b86c52
Corgi <hwanggumedia@gmail.com>
2021-02-22 01:55:51 +0900

    feat: Add progress #14 'Chat Application'

--- chat-application/front-end/src/components/ChatBox/index.js
+++ chat-application/front-end/src/components/ChatBox/index.js
@@ -7,10 +7,12 @@ const colorBlack = "black";
 export function ChatTextArea() {
     const Textarea = styled.textarea`
         border: none;
+        resize: none;
         background-color: ${colorGrey};
         text-decoration: none;
         color: ${colorBlack};
         padding: 1em;
+        border-radius: 5em;
     `;
     return (
         <Textarea/>
